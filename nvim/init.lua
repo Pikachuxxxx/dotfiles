da14b87be3570823cb07e23b434b49b288bc4863
Parth Saravade <parthsaravade.acad@gmail.com>
2024-01-23 23:10:36 +0530

    remove more useless code

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,19 +70,6 @@ plugins = {
 }
 }
 -- CONFIG
--- no clown fiesta
-require("no-clown-fiesta").setup({
-	transparent = false, -- Enable this to disable the bg color
-	styles = {
-		-- You can set any of the style values specified for `:h nvim_set_hl`
-		comments = {},
-		keywords = {},
-		functions = {},
-		variables = {},
-		type = { bold = true },
-		lsp = { underline = true }
-	},
-})
 -- lazy
 require("lazy").setup(plugins,opts)
 
